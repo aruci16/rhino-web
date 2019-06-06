dbe91a2f94d2dd85538641b8d39672ecfd24cdec
Grent <grentmustafa@gmail.com>
2019-06-06 10:30:07 +0200

    adding view for Business and HomePage

---
+++ view/business-home.php
@@ -0,0 +1,166 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css">
+    <link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+</head>
+<body>
+
+<div class="clearfix">
+
+    <header id="header" class="black">
+
+        <div class="container clearfix">
+
+            <div id="logo">
+                <a href="index.html"><img src="images/logo1.png" alt="Canvas Logo"></a>
+            </div>
+
+            <nav id="primary-menu">
+
+                <ul  style="touch-action: pan-y;">
+                    <li class=""><a href="#"><div>My Products</div></a></li>
+                    <li><a href="#"><div>Add Product</div></a></li>
+                    <li><a href="#"><div>Customers</div></a></li>
+                    <li><a href="#"><div>My Account</div></a></li>
+                    <li><a href="#"><div>Log Out</div></a></li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+    </header>
+</div>
+<section style="margin-bottom: 0px;" id="">
+
+    <div class="content-wrap">
+
+        <div class="container clearfix">
+
+            <div class="">
+                <div id="" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"><div id="" class="dataTables_length"><label>Show <select name="datatable1_length" aria-controls="datatable1" class=""><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"><div id="" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="datatable1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="datatable1" role="grid" aria-describedby="datatable1_info" style="width: 100%;" class="table table-striped table-bordered dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                <tr role="row">
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 183px;" aria-label="Name: activate to sort column descending" class="">Product Name</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 276px;" aria-label="Position: activate to sort column ascending">Category</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 129px;" aria-label="Office: activate to sort column ascending" class="">Discount</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 65px;" aria-label="Age: activate to sort column ascending">Start date</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 124px;" aria-label="Start date: activate to sort column ascending">End date</th><th tabindex="0" rowspan="1" colspan="1" style="width: 109px;" aria-label="Salary: activate to sort column ascending">Price</th></tr>
+                                </thead>
+                                <tfoot>
+                                <tr><th rowspan="1" colspan="1">Product Name</th><th rowspan="1" colspan="1">Category</th><th rowspan="1" colspan="1">Discount</th><th rowspan="1" colspan="1">Start date</th><th rowspan="1" colspan="1">End date</th><th rowspan="1" colspan="1">Price</th></tr>
+                                </tfoot>
+                                <tbody>
+                                <tr role="row" class="odd">
+                                    <td>Product1</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                    <td>33</td>
+                                    <td>2008/11/28</td>
+                                    <td>$162,700</td>
+                                </tr><tr role="row" class="even">
+                                    <td>Product2</td>
+                                    <td>Chief Executive Officer (CEO)</td>
+                                    <td>London</td>
+                                    <td>47</td>
+                                    <td>2009/10/09</td>
+                                    <td>$1,200,000</td>
+                                </tr><tr role="row" class="odd">
+                                    <td class="sorting_1">Product3</td>
+                                    <td>Junior Technical Author</td>
+                                    <td>San Francisco</td>
+                                    <td>66</td>
+                                    <td>2009/01/12</td>
+                                    <td>$86,000</td>
+                                </tr><tr role="row" class="even">
+                                    <td class="sorting_1">Product4</td>
+                                    <td>Software Engineer</td>
+                                    <td>London</td>
+                                    <td>41</td>
+                                    <td>2012/10/13</td>
+                                    <td>$132,000</td>
+                                </tr><tr role="row" class="odd">
+                                    <td class="sorting_1">Product5</td>
+                                    <td>Software Engineer</td>
+                                    <td>San Francisco</td>
+                                    <td>28</td>
+                                    <td>2011/06/07</td>
+                                    <td>$206,850</td>
+                                </tr><tr role="row" class="even">
+                                    <td class="sorting_1">Product6</td>
+                                    <td>Integration Specialist</td>
+                                    <td>New York</td>
+                                    <td>61</td>
+                                    <td>2012/12/02</td>
+                                    <td>$372,000</td>
+                                </tr><tr role="row" class="odd">
+                                    <td class="sorting_1">Product7</td>
+                                    <td>Software Engineer</td>
+                                    <td>London</td>
+                                    <td>38</td>
+                                    <td>2011/05/03</td>
+                                    <td>$163,500</td>
+                                </tr><tr role="row" class="even">
+                                    <td class="sorting_1">Product8</td>
+                                    <td>Pre-Sales Support</td>
+                                    <td>New York</td>
+                                    <td>21</td>
+                                    <td>2011/12/12</td>
+                                    <td>$106,450</td>
+                                </tr><tr role="row" class="odd">
+                                    <td class="sorting_1">Product9</td>
+                                    <td>Sales Assistant</td>
+                                    <td>New York</td>
+                                    <td>46</td>
+                                    <td>2011/12/06</td>
+                                    <td>$145,600</td>
+                                </tr><tr role="row" class="even">
+                                    <td class="sorting_1">Product10</td>
+                                    <td>Senior Javascript Developer</td>
+                                    <td>Edinburgh</td>
+                                    <td>22</td>
+                                    <td>2012/03/29</td>
+                                    <td>$433,060</td>
+                                </tr></tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-5">
+                            <div class="dataTables_info" id="datatable1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries
+                            </div></div><div class="col-sm-12 col-md-7">
+                            <div class="dataTables_paginate paging_simple_numbers">
+                                <ul class="pagination">
+                                    <li class="paginate_button page-item previous disabled" id="datatable1_previous"><a href="#" aria-controls="datatable1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a>
+                                    </li>
+                                    <li class="paginate_button page-item active"><a href="#" aria-controls="datatable1" data-dt-idx="1" tabindex="0" class="page-link">1</a>
+                                    </li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="datatable1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="datatable1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="datatable1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="datatable1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                                    <li class="paginate_button page-item "><a href="#" aria-controls="datatable1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                                    <li class="paginate_button page-item next" id="datatable1_next"><a href="#" aria-controls="datatable1" data-dt-idx="7" tabindex="0" class="page-link">Next</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+</body>
+</html>
---
+++ view/business-login.php
@@ -0,0 +1,63 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Business Login</title>
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+</head>
+<body class="bg">
+
+
+<div class="center">
+    <a href="../index.php"><img src="images/logo1.png" alt="Canvas Logo"></a>
+</div>
+<div style="max-width: 400px; background-color: rgba(255,255,255,0.93);" class="divcenter noradius noborder">
+    <div style="padding: 40px;">
+        <form action="login" method="post" enctype="multipart/form-data">
+            <h3>Login to your Account</h3>
+
+            <div class="col_full float-none">
+                <label for="login-form-username">Username:</label>
+                <input type="text" id="inputEmail" class="form-control" placeholder="Enter Username"
+                       required autofocus value="<?php
+                if (isset($_POST["username"]))
+                    echo $_POST["username"];
+                ?>" name="username" placeholder="Enter Username">
+
+            </div>
+
+            <div class="col_full float-none">
+                <label for="">Password:</label>
+                <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control"
+                           placeholder="Password"
+                           required name="pswd">
+                </div>
+            </div>
+
+            <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <p style="color: red">
+                    <?php
+                    if (isset($messageError)) {
+                        echo $messageError;
+                    }
+                    ?>
+                </p>
+            </div>
+
+            <div class="col_full float-none">
+                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit"
+                        name="login">Sign in
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
